f94c6ea1e61896493ffdc3131bae7619deef4c74
jiangrains <jiangrains@126.com>
2016-07-10 21:00:21 +0800

    1.首次上传文件

---
+++ apps/account/models.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Account(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __unicode__(self):
+    	return self.username
