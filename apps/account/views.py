d8eea649ab6dcdc6cbf5c52aaac90b735b990dd2
jiangrains <jiangrains@126.com>
2016-07-11 15:53:02 +0800

    1.view.py 添加解析post请求，返回json数据的示例

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+import logging
 
 # Create your views here.
 
+@csrf_exempt
 def login(request):
-	return HttpResponse("Hello world.")
+	username=request.POST.get('username','')
+	password=request.POST.get('password','')
+
+	logging.debug("username:%s password:%s" % (username, password))
+
+
+	response_data = {}
+	response_data['username'] = 'jiangdunchuan'
+	response_data['password'] = '309Jiang'
+
+	return HttpResponse(json.dumps(response_data), content_type = 'application/json')
