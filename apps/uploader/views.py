97aad2e3468f9010aa7ae50fcebedc96f08b3d77
jiangrains <jiangrains@126.com>
2016-07-18 17:44:06 +0800

    Update views.py

--- apps/uploader/views.py
+++ apps/uploader/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 
+from .forms import UploadFileForm
+
 
 # Create your views here.
 
 @csrf_exempt
 def upload(request):
+	if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
 	return HttpResponseRedirect('/static/login.html')
