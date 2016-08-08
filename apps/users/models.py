cef7ea952b2ed9c9d21ad38976dca2f449fea4c5
jiangrains <jiangrains@126.com>
2016-08-08 17:43:18 +0800

    Update models.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -11,7 +11,7 @@ class BankAccount(models.Model):
 	code = models.CharField(max_length=256) #不同国家和地区，其code含义不同
 
 
-class Entiny(models.Model):
+class User(models.Model):
 	nick = models.CharField(max_length=256)
 	avatar = models.CharField(max_length=1024) #TODO待后续处理
 	region = models.IntegerField()
@@ -23,3 +23,42 @@ class Entiny(models.Model):
 
 	def __unicode__(self):
    		return self.nick
+
+class Individual(models.Model):
+	user = models.OneToOneField(User)
+	name = models.CharField(max_length=256)
+	company = models.CharField(max_length=256)
+
+	def __unicode__(self):
+   		return ("Individual:" + self.name)
+
+
+class Group(models.Model):
+	user = models.OneToOneField(User)
+	name = models.CharField(max_length=256)
+
+	def __unicode__(self):
+   		return ("Group:" + self.name)
+
+
+class Company(models.Model):
+	user = models.OneToOneField(User)
+	name = models.CharField(max_length=256)
+	photo = models.CharField(max_length=1024) #TODO营业执照文件地址
+	code = models.CharField(max_length=256) #组织机构代码
+	tax_registration = models.CharField(max_length=1024) #TODO税务登记证文件地址
+
+	def __unicode__(self):
+   		return ("Company:" + self.name)
+
+
+class Skill(models.Model):
+	user = models.ForeignKey(User)
+	name = models.CharField(max_length=32) #技能名称
+	like_cnt = models.IntegerField() #该技能被点赞次数
+
+
+class LikeRecord(models.Model):
+	skill = models.ForeignKey(Skill)
+	user = models.ForeignKey(User) #点赞的用户
+	like_date = models.DateTimeField(auto_now=False) #点赞的时间戳
