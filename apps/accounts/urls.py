5cdc05ab894f32e9ecde98176a182d2fe27340ec
jiangrains <jiangrains@126.com>
2016-08-08 17:41:55 +0800

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^login.json$', views.login),
+    url(r'^signup$', views.signup),
+    url(r'^signin$', views.signin),
+    url(r'^checktoken$', views.checktoken),
+    url(r'^activate$', views.activate),
+    url(r'^exists$', views.exists),
+    url(r'^retrieve$', views.retrieve),
+    url(r'^resetpsw$', views.resetpsw),
 ]
