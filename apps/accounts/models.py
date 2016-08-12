a6ea040ec28dba87d1bd01a1fe12fa1f862aec7c
jiangrains <jiangrains@126.com>
2016-08-12 17:51:41 +0800

    Update models.py

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,6 +5,11 @@ from django.db import models
 from users.models import User
 
 import django.utils.timezone as timezone
+import datetime
+
+class Utils():
+    def get_random_string(len):
+        return ''.join(random.sample(string.ascii_letters + string.digits, len))  
 
 
 class AccountManager(models.Manager):
@@ -19,6 +24,8 @@ class Account(models.Model):
     ACCOUNT_NOTACTIVE = 0
     ACCOUNT_ACTIVE = 1
     ACCOUNT_LOCKED = 2
+
+    TOKEN_EXPIRE_DAYS = (2*7) #2 weeks
     
     #user = models.OneToOneField(User)
     email = models.CharField(max_length = 256) #注册邮箱
@@ -26,7 +33,7 @@ class Account(models.Model):
     status = models.IntegerField(default = ACCOUNT_NOTACTIVE) #账号状态，0：未激活，1：正常，2：其他状态
     phone = models.CharField(max_length = 32) #若绑定手机，则为手机号；否则为空
     signup_date = models.DateTimeField(auto_now_add = True) #注册时间 (import django.utils.timezone as timezone ; default = timezone.now)
-    sign_in = models.DateTimeField(auto_now = True) #最后登录时间
+    signin_date = models.DateTimeField(auto_now = True) #最后登录时间
     code = models.CharField(max_length = 256) #激活码
     code_expire_date = models.DateTimeField(default = timezone.now) #激活码过期时间
     token = models.CharField(max_length = 256) #令牌
@@ -39,6 +46,16 @@ class Account(models.Model):
     def __init__(self, *args, **kwargs):
         super(Account, self).__init__(*args, **kwargs)
         #user = None
+
+    def remember(self, remember):
+        if remember == True:
+            self.token = Utils.get_random_string(32)
+            self.token_expire_date = timezone.now + datetime.timedelta(days = TOKEN_EXPIRE_DAYS)
+        else:
+            self.token = ""
+        return self.token    
+
+            
 
 
 
